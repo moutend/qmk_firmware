5cbc6aea3345774e04ec6ed7d9a96bc235a79033
Yoshiyuki Koyanagi <moutend@gmail.com>
2016-06-20 10:13:01 +0900

    Modify to use audio vol up and down
    Note: KC_VOLUP and KC_VOLDOWN are not working onw Windows 10
    KC_AUDIO_VOL_UP and vice versa work on both Windows and Mac OSX

--- keyboard/ergodox_ez/keymaps/moutend/keymap.c
+++ keyboard/ergodox_ez/keymaps/moutend/keymap.c
@@ -32,23 +32,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        LT(SYMB,KC_GRV),  KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_F2,
+        LT(SYMB,KC_GRV),  KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_F4,
         KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_SPACE,
         KC_CAPS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,   KC_SPACE,
-        KC_ESC,KC_LCTRL,      KC_BSPACE,  KC_LALT,KC_LGUI,
-                                              KC_LANG1,  KC_LANG2,
-                                                              KC_KANA,
-                                               KC_SPACE, KC_LCTRL, KC_LGUI,
+        KC_ESC,KC_LCTRL,      KC_LCTRL,  KC_LALT,KC_LGUI,
+                                              KC_LSFT, KC_LALT,
+                                                              KC_AUDIO_VOL_UP,
+                                               KC_SPACE, KC_BSPC, KC_AUDIO_VOL_DOWN,
         // right hand
              KC_F5,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              KC_SPACE,    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_EQL,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
              KC_SPACE,KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_LBRC,KC_RBRC,KC_BSLS,KC_RALT,          KC_ENT,
+                                  KC_LBRC,KC_RBRC,KC_BSLS,KC_RGUI,KC_RALT,
              KC_LEFT, KC_RIGHT,
              KC_UP,
-             KC_DOWN,KC_RCTRL,  KC_RGUI
+             KC_DOWN,KC_RCTRL,  KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
