864b9fe1aa716141c5d05d82ba7fda9b9c28cbbf
Yoshiyuki Koyanagi <moutend@gmail.com>
2016-06-23 18:21:18 +0900

    Swap KC_RGUI and KC_RCTRL

--- keyboard/ergodox_ez/keymaps/moutend/keymap.c
+++ keyboard/ergodox_ez/keymaps/moutend/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_SPACE,    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_EQL,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
              KC_BSPC,KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_LBRC,KC_RBRC,KC_BSLS,KC_RALT,KC_RGUI,
+                                  KC_LBRC,KC_RBRC,KC_BSLS,KC_RALT,KC_RCTRL,
              KC_LEFT, KC_RIGHT,
              KC_UP,
              KC_DOWN,KC_RALT, KC_ENT
