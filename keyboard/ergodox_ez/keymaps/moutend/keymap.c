d3c3c4eeb8cbaf32a7706cf96f91d9eb634c9063
Yoshiyuki Koyanagi <moutend@gmail.com>
2016-07-09 07:52:16 +0900

    Assign ESC for control VoiceOver

--- keyboard/ergodox_ez/keymaps/moutend/keymap.c
+++ keyboard/ergodox_ez/keymaps/moutend/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                KC_SPACE, KC_LCTRL, KC_AUDIO_VOL_DOWN,
         // right hand
              KC_F5,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-             KC_SPACE,    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_EQL,
+             KC_ESC,    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_EQL,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
              KC_BSPC,KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                                   KC_LBRC,KC_RBRC,KC_BSLS,KC_RALT,KC_RCTRL,
